5a2ef745b7f18c1303991783de8a03995c2695af
Suaib7860 <132182414+Suaib7860@users.noreply.github.com>
2023-05-08 15:15:35 +0530

    Add files via upload

---
+++ Hollow_rectangle.java
@@ -0,0 +1,22 @@
+package Pattern;
+
+public class Hollow_rectangle {
+    public static void main(String args[]) {
+        int n=5;
+        int m=4;
+
+        for(int i=1; i<=n; i++) }
+             for(int j=1; j<=m; j++) {
+                if(i == 0 || i == n-1 || j == 0 || j == m-1) {
+                    System.out.print("*");
+                    } else {
+                    System.out.print(" ");
+                    }
+                    }
+                    System.out.println();             
+                }
+        
+
+    
+    
+
